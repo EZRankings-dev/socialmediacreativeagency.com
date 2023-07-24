afd5c37bd399b7f09570253389c712064697bc1f
EZRankings-dev <abhishek@ezrankings.org>
2023-07-24 14:35:18 +0530

    update redirection issue

--- next.config.js
+++ next.config.js
@@ -11,6 +11,25 @@ module.exports = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/': '',
     // Set basePath to the root path of your website
     basePath: process.env.NODE_ENV === 'production' ? '' : '',
-};
+    async redirects() {
+      return [
+        {
+          source: '/contact-us',
+          destination: '/contact',
+          permanent: true,
+        },
+        {
+          source: '/how-to-promote-your-business-on-instagram-in-2023-10-ways-that-actually-work',
+          destination: '/How-To-Promote-Your-Business-On-Instagram-In-2023-%E2%80%93-10-Ways-That-Actually-Work',
+          permanent: true,
+        },
+        {
+          source: '/7-social-media-hacks-to-double-your-brand-growth-in-2023',
+          destination: '/7-Social-Media-Hacks-To-Double-Your-Brand-Growth-In-2023',
+          permanent: true,
+        }             
+      ]
+    },
+  };
 
 // module.exports = nextConfig
