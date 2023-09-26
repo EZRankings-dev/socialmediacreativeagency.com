816b48ba9f3e816cb3c703d4b3c84b6d8338dc4a
EZRankings-dev <abhishek@ezrankings.org>
2023-09-26 15:44:28 +0530

    update css responsivw

--- .next/static/CYIq7mZvS4U8SEfWPXT0C/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F[blogdetail]","\u002Fabout","\u002Fblog\u002F[blogdetail]","\u002Fblogs","\u002Fpricing","\u002Fservices"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
